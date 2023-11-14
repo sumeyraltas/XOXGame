b57eee0106a9fb71d6506cc005931b56ae753429
sumeyraltas <sumeyraltas@gmail.com>
2023-11-14 16:36:31 +0300

    Initial Commit

---
+++ XOXGame/XOXGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOXGameApp.swift
+//  XOXGame
+//
+//  Created by Sumeyra Altas on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct XOXGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
